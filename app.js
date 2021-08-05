a6a35c67257b6bac75015324a35abca6de8a84fa
gunda <gundakamalnath@gmail.com>
2021-08-05 16:42:29 +0530

    API key hidden

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-const API_KEY = "dict.1.1.20210216T114936Z.e4989dccd61b9626.373cddfbfb8a3b2ff30a03392b4e0b076f14cff9";
+const API_KEY = require("./api.js").API_KEY;
 
 const matchingWord = "test"; //word of which count is to be found
 
